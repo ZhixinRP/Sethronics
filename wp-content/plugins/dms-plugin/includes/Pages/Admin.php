8c1390b072947aea1924486dbbae674973624175
Zhixin <liuzhixin.sg@gmail.com>
2022-06-20 21:55:43 +0800

    added the admin pages and subpages

--- wp-content/plugins/dms-plugin/includes/Pages/Admin.php
+++ wp-content/plugins/dms-plugin/includes/Pages/Admin.php
@@ -4,20 +4,66 @@
  */
 namespace Inc\Pages;
 
+use \Inc\Base\BaseController;
+use \Inc\Api\SettingsApi;
+
 /**
 * 
 */
-class Admin
+class Admin extends BaseController
 {
-	public function register() {
-		add_action( 'admin_menu', array( $this, 'add_admin_pages' ) );
+	public $settings;
+
+	public $pages = array();
+
+	public $subpages = array();
+
+	public function __construct()
+	{
+		$this->settings = new SettingsApi();
+
+		$this->pages = array(
+			array(
+				'page_title' => 'DMS Plugin', 
+				'menu_title' => 'DMS', 
+				'capability' => 'manage_options', 
+				'menu_slug' => 'dms_plugin', 
+				'callback' => function() { echo '<h1>Dms Plugin</h1>'; }, 
+				'icon_url' => 'dashicons-car', 
+				'position' => 110
+			)
+		);
+
+		$this->subpages = array(
+			array(
+				'parent_slug' => 'dms_plugin', 
+				'page_title' => 'Custom Post Types', 
+				'menu_title' => 'CPT', 
+				'capability' => 'manage_options', 
+				'menu_slug' => 'dms_cpt', 
+				'callback' => function() { echo '<h1>CPT Manager</h1>'; }
+			),
+			array(
+				'parent_slug' => 'dms_plugin', 
+				'page_title' => 'Custom Taxonomies', 
+				'menu_title' => 'Taxonomies', 
+				'capability' => 'manage_options', 
+				'menu_slug' => 'dms_taxonomies', 
+				'callback' => function() { echo '<h1>Taxonomies Manager</h1>'; }
+			),
+			array(
+				'parent_slug' => 'dms_plugin', 
+				'page_title' => 'Custom Widgets', 
+				'menu_title' => 'Widgets', 
+				'capability' => 'manage_options', 
+				'menu_slug' => 'dms_widgets', 
+				'callback' => function() { echo '<h1>Widgets Manager</h1>'; }
+			)
+		);
 	}
 
-	public function add_admin_pages() {
-		add_menu_page( 'dms', 'DMS', 'manage_options', 'dms_plugin', array( $this, 'admin_index' ), 'dashicons-car', 110 );
-	}
-
-	public function admin_index() {
-		require_once PLUGIN_PATH . 'templates/admin.php';
+	public function register() 
+	{
+		$this->settings->addPages( $this->pages )->withSubPage( 'Dashboard' )->addSubPages( $this->subpages )->register();
 	}
 }
