de6dd56be01a11131ddc2419d02bd237d2f3c2b2
Zikai <20014063@myrp.edu.sg>
2022-07-27 09:42:05 +0800

    Changes to mobile responsive, View and filter assign orders function added

--- wp-content/plugins/delivery-management-system/admin/partials/dms-dp-orders.php
+++ wp-content/plugins/delivery-management-system/admin/partials/dms-dp-orders.php
@@ -16,6 +16,16 @@ update_dms_table();
 global $wpdb;
 $table_name = $wpdb->prefix . "dms_orders";
 
+
+
+// Email Section
+
+$to = "liangzikai1111@gmail.com";
+$subject = "test";
+$txt = "Testing";
+$headers = "From: The Sender Name <sethtroinics@sethtronics.com\r\n";
+
+
 if (isset($_POST['accept_order'])) {
     $order_id = $_POST['accept_order'];
     $sql = $wpdb->prepare("UPDATE $table_name SET is_accepted = 1 WHERE order_id = '$order_id'");
@@ -25,6 +35,9 @@ if (isset($_POST['accept_order'])) {
     } else {
         echo "<p id='alert' class='alert alert-danger'>" . $wpdb->last_error . "</p>";
     }
+
+    mail($to,$subject,$txt, $headers);
+
 }
 if (isset($_POST['reject_order'])) {
     $order_id = $_POST['reject_order'];
@@ -50,6 +63,8 @@ if (isset($_POST['update_order'])) {
         echo "<p id='alert' class='alert alert-danger'>" . $wpdb->last_error . "</p>";
     }
 }
+
+
 
 ?>
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet">
